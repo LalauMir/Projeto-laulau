278fa65c65a7020b4839fddd038561709288e110
LalauMir <ladislauzinho78@gmail.com>
2023-11-06 08:02:23 -0300

    :tada: initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,11 @@
+function togglemode() {
+    const hmtl= document.documentElement
+
+//  if(html.classList.contains('light')){
+//html.classList.remove('light')
+//}else{
+//html.classList.add('light')
+//}
+
+hmtl.classList.toggle('light')
+}
